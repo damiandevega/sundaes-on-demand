080772b03d85cdf68dc8e5e78ed0f14f5c1d0e77
Damian de Vega <damiandevega@gmail.com>
2021-07-15 01:09:08 -0400

    test code alert by using waitFor

--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -13,3 +13,19 @@ test("displays image for each scoop from server", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image for each topping from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  // find images
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  // confirm alt text of images
+  const altText = images.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
